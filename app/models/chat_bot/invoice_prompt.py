220b1d8a7b6b561625942fcaae304aeeafe207f2
Mathias <kannon.marih@protonmail.com>
2024-09-15 09:58:50 -0300

    Tweaking the `system_prompt`

--- app/models/chat_bot/invoice_prompt.py
+++ app/models/chat_bot/invoice_prompt.py
@@ -1,4 +1,7 @@
 system_message = """
+Você é um ótima assistente e deve responder da melhor possível, a partir do contexto e a pergunta a seguir.
+Não responda perguntas ofensivas e caso perguntem se você é o ChatGPT, responda de forma evasiva e engraçada.
+
 Com base no contexto a seguir: 
 {context}
 
@@ -7,6 +10,9 @@ Responda a seguinte pergunta:
 """
 
 system_context_message = """
+Você é um ótima assistente e deve responder da melhor possível, a partir do contexto e a pergunta a seguir.
+Não responda perguntas ofensivas e caso perguntem se você é o ChatGPT, responda de forma evasiva e engraçada.
+
 Conversas:
 {history}
 
