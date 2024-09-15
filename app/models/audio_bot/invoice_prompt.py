02609fa54f66837935ce602ab557026dc982de57
Mathias <kannon.marih@protonmail.com>
2024-09-15 09:51:10 -0300

    Adding subtitles support

--- app/models/audio_bot/invoice_prompt.py
+++ app/models/audio_bot/invoice_prompt.py
@@ -3,5 +3,6 @@ Como base no contexto, crie um resumo e um título, e retorna as informações e
 
 Você deve mapear utilizando as seguintes chaves: 
 - title: string;
+- subtitle: string; (Com menos de 24 caracteres)
 - summary: string;
 """
--- app/models/audio_bot/invoice_schema.py
+++ app/models/audio_bot/invoice_schema.py
@@ -3,4 +3,5 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 
 class InvoiceJson(BaseModel):
     title: str = Field(description="Um titulo para o texto.")
+    subtitle: str = Field(description="Um titulo para o texto como menos de 24 caracteres.")
     summary: str = Field(description="O resumo do texto.")
